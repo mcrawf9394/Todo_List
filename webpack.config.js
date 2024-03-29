8b83183f03ff941c071b47ca48f33e753bde56d3
mcrawf9394 <mjc9394@gmail.com>
2024-03-29 16:56:48 -0400

    Intial Commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'head',
+            scriptLoading: 'defer',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
