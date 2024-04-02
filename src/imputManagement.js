d2588bd510d6630ba7604589379656071ccdcac7
mcrawf9394 <mjc9394@gmail.com>
2024-04-01 20:09:33 -0400

    Started making arrays for the tasks to be stored in each project and then set it up to where it could be recalled.

--- src/imputManagement.js
+++ src/imputManagement.js
@@ -1,15 +1,101 @@
-class taskButtons {
-    constructor () {
+import display from "./index.js"
+const taskButtons = {
+    taskPopUp: document.getElementById('taskAddition'),
+    taskButton (projectName) {
         let taskButton = document.createElement('button')
         taskButton.textContent = "Add Task"
+        taskButton.className = "taskButton"
+        taskButton.id = projectName
         taskButton.addEventListener('click', (userImput) => {
             userImput.preventDefault()
+            this.taskPopUp.showModal()
         })
+        return taskButton
+    }
+}
+class projects {
+    constructor (projectName) {
+        const project = {
+            name: projectName,
+            tasks: [],
+            completedTasks: [],
+        }
+        return project
+    }
+}
+class tasks {
+    checkPriority () {
+        if (document.getElementById('highPriority').checked == true) {
+            return document.getElementById('highPriority').value
+        }
+        else if (document.getElementById('moderatePriority').checked == true) {
+            return document.getElementById('moderatePriority').value
+        }
+        else {
+            return document.getElementById('lowPriority').value
+        }
+    }
+    constructor () {
+        const task = {
+            taskName: document.getElementById('taskName').value,
+            description: document.getElementById('description').value,
+            date: document.getElementById('date').value,
+            priority: this.checkPriority()
+        }
+        return task
     }
 }
 const projectStorage = {
+    cancel: document.getElementById('cancelTaskAddition'),
+    add: document.getElementById('addTaskButton'),
+    taskPopUp: document.getElementById('taskAddition'),
     projectArray: [],
-    addingProject () {
-        
+    buttonEvents () {
+        this.cancel.addEventListener('click', (click) =>{
+            this.taskPopUp.close()
+            click.preventDefault()
+        })
+        this.add.addEventListener('click', (click) => {
+            click.preventDefault()
+            while (document.getElementById('taskContainer')) {
+            let container = document.getElementById('taskContainer')
+            container.remove()
+            }
+            let task = new tasks ()
+            let currentProject = this.projectArray.find(this.findingProject)
+            currentProject.tasks.push(task)
+            console.log(currentProject.tasks)
+            currentProject.tasks.forEach(taskDisplay.display)
+            this.taskPopUp.close()
+        })
+    },
+    findingProject (project){
+        return project.name == display.currentProject
+    },
+    addingProject (projectName) {
+        let project = new projects (projectName)
+        this.projectArray.push(project)
+        const taskButton = taskButtons.taskButton()
+        return taskButton
     }
 }
+projectStorage.buttonEvents()
+const taskDisplay = {
+    display (item) {
+        let taskName = document.createElement('h2')
+        taskName.textContent = "Task Name: " + item.taskName
+        let taskDescription = document.createElement('h2')
+        taskDescription.textContent = "Description: " + item.description
+        let taskDate = document.createElement('h2')
+        taskDate.textContent = "Date: " + item.date
+        let taskPriority = document.createElement('h2')
+        taskPriority.textContent = "Priority: " + item.priority
+        let container = document.getElementById('taskArea')
+        let taskContainer = document.createElement('div')
+        taskContainer.id = "taskContainer"
+        taskContainer.append(taskName, taskDescription, taskDate,taskPriority)
+        container.appendChild(taskContainer)
+    }
+}
+export default projectStorage;
+export  {taskDisplay};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 import './style.css';
+import projectStorage from './imputManagement.js';
+import {taskDisplay} from './imputManagement.js';
 const elements = {
     projectList: document.getElementById('projectContainer'),
     taskArea: document.getElementById('taskArea'),
@@ -8,6 +10,7 @@ const elements = {
     cancel: document.getElementById('cancelCreation')
 }
 const display = {
+    currentProject: "",
     projectForm: document.querySelector('dialog'),
     addProjectButtonHandling () {
         this.projectForm.showModal()
@@ -16,11 +19,31 @@ const display = {
         this.projectForm.close()
     },
     addProject () {
-        container = document.getElementById('projectContainer')
+        const container = document.getElementById('projectContainer')
         projectName = document.getElementById('projectName').value
+        const projectTab = document.createElement('button')
+        projectTab.className = "projectTabs"
+        projectTab.textContent = projectName
+        container.appendChild(projectTab)
+        const addTask = projectStorage.addingProject(projectName)
+        const title = document.createElement('h2')
+        title.textContent = projectName
+        projectTab.addEventListener('click', () => {
+            if (this.currentProject != projectTab.textContent){
+                while (elements.taskArea.firstChild) {
+                elements.taskArea.removeChild(elements.taskArea.firstChild)
+            }
+                elements.taskArea.appendChild(title)
+                elements.taskArea.appendChild (addTask)
+            }
+            this.currentProject = projectTab.textContent
+            let currentProject = projectStorage.projectArray.find(projectStorage.findingProject)
+            currentProject.tasks.forEach(taskDisplay.display)
+        })
         this.projectForm.close()
     }
 }
+export default display; 
 const reponsivebuttons = {
     clickResponses () {
         elements.addProjectButton.addEventListener('click',(userEntry) => {
